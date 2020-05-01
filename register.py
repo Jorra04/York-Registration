b75ea17084bcb2718a072a2f3335f8567450f6fa
jorra04 <jorra04@my.yorku.ca>
2020-04-30 22:00:56 -0400

    made it work for other courses.

--- register.py
+++ register.py
@@ -31,14 +31,21 @@ class YorkRegistration:
         self.driver.find_element_by_name('5.1.27.1.23').click()
         sleep(5)
         self.driver.find_element_by_name('5.1.27.7.7').send_keys('E96T01') 
+        # self.driver.find_element_by_name('5.1.27.7.7').send_keys('V94K01')
         sleep(3)
         self.driver.find_element_by_name('5.1.27.7.9').click()
         sleep(2)
-        self.driver.find_element_by_name('5.1.27.11.9').click()
+        self.driver.find_element_by_xpath("//input[@type=\"submit\"]").click()
+        # print(self.driver.find_element_by_xpath('/html/body/form/div[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[1]/td[2]/span/font/b').text)
+        if(self.driver.find_element_by_xpath('/html/body/form/div[1]/table/tbody/tr[4]/td[2]/table/tbody/tr/td/table[2]/tbody/tr[1]/td[2]/span/font/b').text != 'The course has not been added.'):
+            yr.enrolled = True
+        else:
+            sleep(900)
 
 yr = YorkRegistration()
 i = 0
-while not yr.enrolled:
+while not yr.enrolled and i < 50:
     i+=1
     print(i)
     yr.enrol(user,pw)
+yr.driver.close
